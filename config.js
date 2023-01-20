80d03637f97fa7fb05c603ab6541c6a233ba2b25
devbaloch12 <122773391+devbaloch12@users.noreply.github.com>
2023-01-20 17:25:42 +0500

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ module.exports = {
     REJECT_CALLS: convertToBool(process.env.REJECT_CALLS) || false,
     PMB: process.env.PMB || '_Personal messages not allowed, BLOCKED!_',
     READ_COMMAND: convertToBool(process.env.READ_COMMAND) || true,
-    SESSION: process.env.SESSION.trim() || 'Raganork~4fb4066aae7b62a617169f6fae03572e:427a71724278536f35716b3d',
+    SESSION: process.env.SESSION.trim() || 'Raganork~a9a6a4cf3c8603e422fa1589cd38ab9d:48554251666443426c616b3d',
     IMGBB_KEY: ["76a050f031972d9f27e329d767dd988f","deb80cd12ababea1c9b9a8ad6ce3fab2","78c84c62b32a88e86daf87dd509a657a"],
     RG: process.env.RG || '919074309534-1632403322@g.us',
     BOT_INFO: process.env.BOT_INFO || 'Raganork;Skl11;0;https://i.imgur.com/P7ziVhr.jpeg;https://chat.whatsapp.com/Dt3C4wrQmt0GG6io1IBIHb',
